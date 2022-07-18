82554d44b277c47163449a43cf0ddcab8a105669
Akira - Cleber Akira Nakandakare <cakira@cpqd.com.br>
2022-07-18 10:58:51 -0300

    Não sei. backup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,20 @@
+function is_wsl1
+    return (uname -r | grep Microsoft > /dev/null)
+end
+
 # This is a configuration for fish running in WSL 2.
 # I disabled the native Windows graphic server (WSLg) because it's still buggy
-# And I'm using VcXsrv as a graphic server.
-
+# And I'm using Vnc
 set -x WIN_HOST (awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null) 
-set -x DISPLAY $WIN_HOST:0
+if is_wsl1
+    set -x DISPLAY 127.0.0.1:0
+else
+    set -x DISPLAY $WIN_HOST:0
+end
 set -x WAYLAND_DISPLAY ""
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/cakira/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
