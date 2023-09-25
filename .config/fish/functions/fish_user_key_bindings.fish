568b4e49ef037c2649e8645631abbee673f6e51a
Akira - Cleber Akira Nakandakare <cakira@cpqd.com.br>
2023-09-25 17:20:05 -0300

    Adiciona mais keybindings

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,8 +1,9 @@
 function fish_user_key_bindings
+    bind -k f1 'task; commandline -f repaint'
     bind -k f3 'echo; timew summary :ids; timew day; commandline -f repaint'
-    bind -k f15 'echo; timew summary; commandline -f repaint' # shit-f3
     bind -k f4 "echo; timew stop;  commandline -f repaint"
     bind -k f9 "git status; commandline -f repaint"
     bind -k f12 "echo -ne '\ec\e[3J\e[H'; commandline -f repaint"
+    bind -k f15 'echo; timew summary; commandline -f repaint' # shit-f3
     bind \cc 'commandline ""'
 end
