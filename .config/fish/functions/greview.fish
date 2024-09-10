801dc04b4a8ab178925d660465a08c2a1856ca13
Akira - Cleber Akira Nakandakare <cakira@cpqd.com.br>
2024-09-10 14:43:30 -0300

    Ajusta configurações do git

--- .config/fish/functions/greview.fish
+++ .config/fish/functions/greview.fish
@@ -1,5 +1,5 @@
 function greview
-    set -l commit "HEAD~$argv[1]"
-    git log -1 $commit --stat
-    git difftool -d $commit~ $commit
+set -l commit $argv[1]
+git log -1 $commit --stat
+git difftool -d $commit~ $commit
 end
